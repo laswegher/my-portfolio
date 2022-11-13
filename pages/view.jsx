7fd3f966a4d286436621e7a6ce021898b1250db5
ufuk tekin <laswegher@gmail.com>
2022-11-13 17:23:59 +0300

    view page created

--- pages/view.jsx
+++ pages/view.jsx
@@ -5,35 +5,35 @@ import Image from 'next/image';
 import TechnologiesCard from '../Components/TechnologiesCard';
 
 const view = () => {
-  // const router = useRouter();
-  // const { id } = router.query;
-  // const selectedProject = project_data.filter((el) => {
-  //   return el.id === Number(id);
-  // });
-  // return (
-  //   <div className="CenterComponent md:mt-0 mt-10">
-  //     <div className="Container pt-14  ">
-  //       {selectedProject.map((el) => {
-  //         return (
-  //           <div key={el.id}>
-  //             <h1 className="text-2xl">{el.title}</h1>
-  //             <div className="w-full flex flex-col md:flex-row gap-5 items-center justify-between">
-  //               <div className="flex-1 w-full">
-  //                 <Image
-  //                   className="w-full h-[500px] mt-6 rounded-xl object-cover"
-  //                   alt={el.title}
-  //                   src={el.image}
-  //                   priority
-  //                 />
-  //               </div>
-  //               <TechnologiesCard tec={el.technologies} />
-  //             </div>
-  //           </div>
-  //         );
-  //       })}
-  //     </div>
-  //   </div>
-  // );
+  const router = useRouter();
+  const { id } = router.query;
+  const selectedProject = project_data.filter((el) => {
+    return el.id === Number(id);
+  });
+  return (
+    <div className="CenterComponent md:mt-0 mt-10">
+      <div className="Container pt-14  ">
+        {selectedProject.map((el) => {
+          return (
+            <div key={el.id}>
+              <h1 className="text-2xl">{el.title}</h1>
+              <div className="w-full flex flex-col md:flex-row gap-5 items-center justify-between">
+                <div className="flex-1 w-full">
+                  <Image
+                    className="w-full h-[500px] mt-6 rounded-xl object-cover"
+                    alt={el.title}
+                    src={el.image}
+                    priority
+                  />
+                </div>
+                <TechnologiesCard tec={el.technologies} />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default view;
